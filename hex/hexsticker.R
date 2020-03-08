48042d4ee81aad50c17e1ce011c8423c23de65c4
Jack H Laverick <jacklaverick@ymail.com>
2020-03-08 20:24:34 +0100

    Upload first package hex sticker.

---
+++ hex/hexsticker.R
@@ -0,0 +1,13 @@
+
+library(hexSticker)
+
+# sticker("polyp2.png", package="MCE", p_size=90, s_x=1.22, s_y= 0.77, s_width = 0.900, s_height = 0.540,
+#         p_x = 0.85, h_fill = "darkslategray", h_color = "orange", filename="sticker1.png", dpi = 700,
+#         url = "package@github.com", u_color = "white", u_size = 15)
+#
+sticker("hex/polyp.png", package="MCE", p_size = 90, s_x=1.22, s_y= 0.77, s_width = 0.900,
+        s_height = 0.540, p_x = 0.85, h_fill = "#003366", h_color = "#CC6600", filename="hex/sticker.png",
+        dpi = 700, url = 'remotes::install_github("Jack-H-Laverick/MCE")', u_color = "white", u_size = 8)
+
+
+
